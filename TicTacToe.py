f1dc4e7176d7dfea04939482d761bcbf4c89e86b
kongas98 <themiskottas98@gmail.com>
2024-04-01 13:33:55 +0300

    Add files via upload

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,7 +1,14 @@
 import random
 
-#the board
-def TicTacboard():
+# Define the initial state of the board
+board = [
+    [" "," "," "],
+    [" "," "," "],
+    [" "," "," "]
+    ]
+
+# Display the Tic-Tac-Toe board
+def TicTacboard(board):
     print("+===+===+===+")
     print("| " + board[0][0] + " | " + board[0][1] + " | " + board[0][2] + " |")
     print("+===+===+===+")
@@ -10,20 +17,19 @@ def TicTacboard():
     print("| " + board[2][0] + " | " + board[2][1] + " | " + board[2][2] + " |")
     print("+===+===+===+")
 
-#define turn order
-def Turn_Selection():
+# Define the function to determine the turn order
+def Turn_Selection(board):
     character =int(input("Would you like to go first or second? Write '1' for first and '2' for second:"))
 
     while character > 2 or character < 1:
         character =int(input("Would you like to go first or second? Write '1' for first and '2' for second:"))
     if character == 1:
-        Player_Selection()
+        Player_Selection(board)
     else:
-        Opponent_Selection()
+        Opponent_Selection(board)
 
-#player selection
-def Player_Selection():
-    pl = 1
+# Function for player's turn
+def Player_Selection(board):
     while True:
         pick = int(input("What cell would you like to mark? (1-9): "))
         while pick < 1 or pick > 9:
@@ -36,49 +42,50 @@ def Player_Selection():
         # Check if the selected cell is empty
         if board[row][col] == " ":
             board[row][col] = "X"
-            win(pl)
-            break
+            TicTacboard(board)
+            if check_winner(board, "X"):
+                print("YOU WIN!")
+                print("GAME OVER")
+                Replay(board)
+            elif is_tie(board):
+                print("It's a tie!")
+                print("GAME OVER")
+                Replay(board)
+            else:
+                Opponent_Selection(board)
+                break
         else:
             print("This cell is already taken. Please pick another one.")
 
 
-#opponent selection        
-def Opponent_Selection():
-    pl = 2
+# Function for opponent's turn       
+def Opponent_Selection(board):
     while True:
         randpick = random.randrange(1,10)
-        # Calculate row and column index based on the pc's pick
         row = (randpick - 1) // 3
         col = (randpick - 1) % 3
 
         # Check if the selected cell is empty
         if board[row][col] == " ":
             board[row][col] = "O"
-            TicTacboard()
-            win(pl)
-            break
+            TicTacboard(board)
+            if check_winner(board, "O"):
+                print("YOU LOSE!")
+                print("GAME OVER")
+                Replay(board)
+            elif is_tie(board):
+                print("It's a tie!")
+                print("GAME OVER")
+                Replay(board)
+            else:
+                Player_Selection(board)
+                break
     
+# Function to check for a tie
+def is_tie(board):
+    return all(board[i][j] != " " for i in range(3) for j in range(3))
 
-def win(player):
-    winner_symbol = None
-    if check_winner(board, "X"):
-        print("YOU WIN!")
-        winner_symbol = "X"
-    elif check_winner(board, "O"):
-        print("YOU LOSE!")
-        winner_symbol = "O"
-
-    if winner_symbol:
-        print("GAME OVER")
-        TicTacboard()
-        quit()
-    else:
-        TicTacboard()
-        if player == 2:
-            Player_Selection()
-        else:
-            Opponent_Selection()
-
+# Function to check winning conditions
 def check_winner(board, symbol):
     for i in range(3):
         # Check rows and columns
@@ -91,25 +98,24 @@ def check_winner(board, symbol):
 
     return False
 
+def Replay(board):
+    replay_input = input("Would you like to play again?(Y/N): ")
+    while replay_input.lower() != "y" and replay_input.lower() != "n":
+        replay_input = input("Would you like to play again?(Y/N): ")
+    if replay_input.lower() == "y":
+        board = [
+            [" "," "," "],
+            [" "," "," "],
+            [" "," "," "]
+            ]
+        TicTacboard(board)
+        Turn_Selection(board)
+    else:
+        quit()
 
-
-
-
-board = [
-    [" "," "," "],
-    [" "," "," "],
-    [" "," "," "]
-    ]
-round = 1
-#Main Programm
+# Main Program
 print ("===TIC TAC TOE===")
-#Call the current board
-TicTacboard()
-#Call the below def to see who plays first
-Turn_Selection()
-
-
-
-
-
-
+# Display the initial board
+TicTacboard(board)
+# Determine the turn order
+Turn_Selection(board)
